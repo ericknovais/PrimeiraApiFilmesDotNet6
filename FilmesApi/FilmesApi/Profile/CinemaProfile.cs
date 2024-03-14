7360694838004fb848c98c71a89b483b18b36162
ericknovais <erick.hora@outlook.com>
2024-03-13 23:53:47 -0300

    Criação da class cinema

---
+++ FilmesApi/FilmesApi/Profile/CinemaProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FilmesApi.Database.Dtos;
+using FilmesApi.Moldels;
+
+namespace FilmesApi.Profile;
+
+public class CinemaProfile : AutoMapper.Profile
+{
+    public CinemaProfile()
+    {
+        CreateMap<CreateCinemaDto, Cinema>();
+        CreateMap<UpdateCinemaDto, Cinema>();
+        CreateMap<Cinema, UpdateCinemaDto>();
+        CreateMap<Cinema, ReadCinemaDto>();
+    }
+}
